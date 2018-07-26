ce55d1ece45dcd05a3c16bdce71862d863cf6e27
ja844h <ja844h@att.com>
2018-07-26 15:10:45 -0500

    initial commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyAisPDC4Lq_hMIQtk_NW5UUF6_lCtn4Doo",
+    authDomain: "lit-fd7fc.firebaseapp.com",
+    databaseURL: "https://lit-fd7fc.firebaseio.com",
+    storageBucket: "gs://lit-fd7fc.appspot.com",
+    messagingSenderId: "100394463814"
+};
+
